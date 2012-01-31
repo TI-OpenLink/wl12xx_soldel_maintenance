TI-OpenLink/wl18xx

wl12xx: consider common encryption type (WEP) for auto-arp
(should be squashed into the other auto-arp patches)

Signed-off-by: Eliad Peller <eliad@wizery.com>

drivers/net/wireless/wl12xx/main.c
@@ -3288,10 +3288,11 @@ static int wl1271_op_set_key(struct ieee80211_hw *hw, enum set_key_cmd cmd,
 		}
 
 		/*
-		 * reconfiguring arp response if the unicast encryption
-		 * type was changed
-		*/
-		if (sta && wlvif->bss_type == BSS_TYPE_STA_BSS &&
+		 * reconfiguring arp response if the unicast (or common)
+		 * encryption key type was changed
+		 */
+		if (wlvif->bss_type == BSS_TYPE_STA_BSS &&
+		    (sta || key_type == KEY_WEP) &&
 		    wlvif->encryption_type != key_type) {
 			wlvif->encryption_type = key_type;
 			ret = wl1271_cmd_build_arp_rsp(wl, wlvif);

TI-OpenLink/wl12xx_soldel_maintenance

Conflicts:
	drivers/net/wireless/wl12xx/main.c
[R4 backport of Eliad's patch]
Signed-off-by: Pontus Fuchs <pontus.fuchs@gmail.com>

drivers/net/wireless/wl12xx/main.c
@@ -3124,10 +3124,11 @@ static int wl1271_op_set_key(struct ieee80211_hw *hw, enum set_key_cmd cmd,
 		}
 
 		/*
-		 * reconfiguring arp response if the unicast encryption
-		 * type was changed
-		*/
-		if (sta && wl->bss_type == BSS_TYPE_STA_BSS &&
+		 * reconfiguring arp response if the unicast (or common)
+		 * encryption key type was changed
+		 */
+		if (wl->bss_type == BSS_TYPE_STA_BSS &&
+		    (sta || key_type == KEY_WEP) &&
 		    wl->encryption_type != key_type) {
 			wl->encryption_type = key_type;
 			ret = wl1271_cmd_build_arp_rsp(wl, wl->ip_addr);