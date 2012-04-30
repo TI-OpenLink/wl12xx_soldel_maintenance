ddce2acf77d1491ba800340e83eb00a4baf81c81
Tomer Kariv <tomer.kariv@ti.com>
2012-04-30 15:54:36 +0300

    wl12xx: don't enable BET for high basic rates
    The beacon early termination is not relevant for
    high basic rates, which doesn't contribute
    anything to the PS and only adds unnecessary FW
    work.
    
    Enable BET only if the basic rate is less than 9
    Mbps

--- drivers/net/wireless/wl12xx/event.c
+++ drivers/net/wireless/wl12xx/event.c
@@ -137,7 +137,7 @@ static int wl1271_event_ps_report(struct wl1271 *wl,
 		 * BET has only a minor effect in 5GHz and masks
 		 * channel switch IEs, so we only enable BET on 2.4GHz
 		*/
-		if (wl->band == IEEE80211_BAND_2GHZ)
+		if ((wl->band == IEEE80211_BAND_2GHZ) && (wl->basic_rate < CONF_HW_BIT_RATE_9MBPS))
 			/* enable beacon early termination */
 			ret = wl1271_acx_bet_enable(wl, true);
 
